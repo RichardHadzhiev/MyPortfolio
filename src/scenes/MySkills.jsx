56683a073e1237948f0829e3254107a72444573a
Richard Hadzhiev <ri4koto100@gmail.com>
2024-06-26 14:12:39 +0200

    finish MySkills

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -28,6 +28,26 @@ const MySkills = () => {
             MongoDB,
           </p>
         </motion.div>
+        <div className="mt-16 md:mt-0">
+          {isAboveMediumScreen ? (
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10 
+              before:w-full  before:h-full before:border-2 before:border-blue before:z-[-1]"
+            >
+              <img
+                alt="skills"
+                className="z-10"
+                src="src/assets/skills-image.png"
+              />
+            </div>
+          ) : (
+            <img
+              alt="skills"
+              className="z-10"
+              src="src/assets/skills-image.png"
+            />
+          )}
+        </div>
       </div>
     </section>
   );
